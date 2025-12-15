8b545ddc43f63ff004f8113239d027de9e488506
Tahiel Ludi <54589060+Tahiel1@users.noreply.github.com>
2025-12-15 18:39:09 -0300

    Add comment in Start method of SS.csprubaa

--- Assets/SS.cs
+++ Assets/SS.cs
@@ -5,7 +5,7 @@ public class SS : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //ddd
     }
 
     // Update is called once per frame
